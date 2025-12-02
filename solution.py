89a1a2cd167848ed0254cbb994b87197a3ddb0f1
LukaDaniels <tw24381@bristol.ac.uk>
2025-12-02 13:35:50 +0000

    Update solution.py

--- solution.py
+++ solution.py
@@ -48,7 +48,7 @@ shootouts_clean = shootouts_clean[~shootouts_clean['data_quality_issue']]
 
 # 1. Average Goals per game between 1900-2000
 
-filtered = results[(results['year'] >= 1900) & (results['year'] <= 2000)] # classify timeframe
+filtered = results_clean[(results_clean['year'] >= 1900) & (results_clean['year'] <= 2000)] # classify timeframe
 filtered['total_goals'] = filtered['home_score'] + filtered['away_score'] # create new column and work out its mean
 average_goals = filtered['total_goals'].mean()
 
@@ -56,7 +56,7 @@ print("1. Average goals per game (1900–2000):", round(average_goals, 2))  # pr
 
 # 2. Number of shootout wins by country
 
-shootout_wins = shootouts['winner'].value_counts().sort_index() 
+shootout_wins = shootouts_clean['winner'].value_counts().sort_index() 
 # Count how many times each country appears as "winner" and sort them alphabetically
 
 print("\n2. Shootout wins by country (alphabetical):")
@@ -72,18 +72,18 @@ def create_key(df):
     )
 # Creates a unique ID for each match, as each CSV file contains these columns
 
-results['join_key'] = create_key(results)
-goalscorers['join_key'] = create_key(goalscorers)
-shootouts['join_key'] = create_key(shootouts)
+results_clean['join_key'] = create_key(results_clean)
+goalscorers_clean['join_key'] = create_key(goalscorers_clean)
+shootouts_clean['join_key'] = create_key(shootouts_clean)
 # Use the function on each CSV
 
 
 
 # 4. Teams that won a shootout after 1-1 draw
 
-draws = results[(results['home_score'] == 1) & (results['away_score'] == 1)] # filters matches that had 1-1 draw
+draws = results_clean[(results_clean['home_score'] == 1) & (results_clean['away_score'] == 1)] # filters matches that had 1-1 draw
 
-merged = draws.merge(shootouts, on='join_key', how='inner') # merges table we just created with shootout table
+merged = draws.merge(shootouts_clean, on='join_key', how='inner') # merges table we just created with shootout table
 
 winners_after_1_1 = merged['winner'].unique() # creates a list of unique winners, removing duplicates
 
@@ -92,7 +92,7 @@ print(winners_after_1_1)
 
 # 5. Top scorer of each tournament, and their % share of goals
 
-goals_with_tournament = goalscorers.merge(
+goals_with_tournament = goalscorers_clean.merge(
     results[['join_key', 'tournament']],
     on='join_key',
     how='left'
